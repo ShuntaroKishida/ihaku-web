acea56484d68cd23033993f3f5e9e7ab724e68e0
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-08-31 16:55:40 +0900

    素材の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tweets#index'
-  resources :tweets
+  root to: 'home#index'
+  resources :tweets, only: [:index]
+  resources :home, only: [:index]
+  resources :intro, only: [:index]
+  resources :schedule, only: [:index]
+  resources :lesson, only: [:index]
+  resources :access, only: [:index]
+  resources :blog, only: [:index]
 end
