5fae1f661bc6938d5ce184228486cc40bb457218
ShuntaroKishida <137754393+ShuntaroKishida@users.noreply.github.com>
2023-09-06 10:14:52 +0900

    変更完了

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :authenticate_with_basic_auth, only: [:new, :create]
+  before_action :authenticate_admin, only: [:create]
 
   private
 
-  def authenticate_with_basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+  def authenticate_admin
+    unless admin_authenticated?
+      redirect_to root_path, alert: "管理者以外は新規登録できません。"
     end
+  end
+
+  def admin_authenticated?
+    admin_password = ENV['ADMIN_PASSWORD']
+    params[:user][:admin_password] == admin_password
   end
 end
